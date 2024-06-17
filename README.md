85ee43cb43abe3d10a3f06a757c436b2edbe6ee9
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2024-06-17 14:44:15 +0530

    readme added

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-# OSOSTaskAngular
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.2.
-
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Make sure start Back-end on port 5237.
 
-## Code scaffolding
+Run Front-End Angular on port 4200.
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+`npm install` - to install dependencies
 
-## Build
+`ng serve` - to run local dev server.
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+Navigate to `http://localhost:4200/`.
 
 ## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `ng test`
 
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+With code-coverage `ng test --code-coverage`
--- src/app/services/date-data.service.spec.ts
+++ src/app/services/date-data.service.spec.ts
@@ -1,17 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DateDataService } from './date-data.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { DateRequestDto } from '../dtos/date-request-dto';
+import { DateResponseDto } from '../dtos/date-response-dto';
 
 describe('DateDataService', () => {
   let service: DateDataService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(DateDataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
@@ -19,13 +29,26 @@ describe('DateDataService', () => {
   });
 
   it('should get end date', () => {
+    const url = 'http://localhost:5237' + '/api/date';
+
     const dateRequestDto: DateRequestDto = {
-      startDate: new Date('2024-06-17').toISOString(),
-      workingDays: 5,
+      startDate: '2024-06-15',
+      workingDays: 1,
+    };
+    const responseData: DateResponseDto = {
+      endDate: '2024-06-12',
+      dayDetails: [
+        { date: '2024-06-12', dayOfWeek: 'Wednesday', isHoliday: false },
+      ],
     };
 
     service.getEndDate(dateRequestDto).subscribe((response) => {
       expect(response.endDate).toBeTruthy();
+      expect(response).toEqual(responseData);
     });
+
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toBe('POST');
+    req.flush(responseData);
   });
 });
